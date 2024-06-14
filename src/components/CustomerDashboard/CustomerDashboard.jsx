e0a26dc2f311e2735390aaa85afa919343d75440
Krzysztof Anwailer <lcplayer@o2.pl>
2024-06-14 23:21:41 +0200

    userDarshboard protected routes

--- src/components/CustomerDashboard/CustomerDashboard.jsx
+++ src/components/CustomerDashboard/CustomerDashboard.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../CustomerDashboard/CustomerDashboard.module.css';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../context/UserContext/UserContext';
 
 const CustomerDashboard = () => {
+
   return (
     <div className={styles.container}>
 
       <div className={styles.panel}>
-        <Link to="/dashboard/customerOrders" className={styles.link}>
+        <Link to="/user/orders" className={styles.link}>
           <p>Zamówienia</p>
         </Link>
 
-        <Link to="/dashboard/customerDataEdit" className={styles.link}>
+        <Link to="/user/data" className={styles.link}>
           <p>Edytuj dane</p>
         </Link>
-        
-        
+
+
       </div>
 
       <div className={styles.main}>
