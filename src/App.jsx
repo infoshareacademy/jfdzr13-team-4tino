7d14423cdc53bada3fb29272a0fa3ada00e676c8
KrystianLewandowski0 <krystian.j.lewandowski.praca@gmail.com>
2024-06-14 21:00:48 +0200

    Fixed the issue with order page rendering in scrolled position after clicking second button on home page - added & imported ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register/Register";
 import Order from "./components/Order/Order";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div>
         <Navbar />
         <Routes>
