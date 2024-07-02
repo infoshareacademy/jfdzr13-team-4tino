bd29bb9c07bc80928c9852f031ccefdd6e0c5848
Marta Kozikowska <147720654+martakozikowska@users.noreply.github.com>
2024-07-02 15:49:29 +0200

    Timer fixed

--- src/components/CustomerDashboard/CountDownTimer/CountDownTimer.jsx
+++ src/components/CustomerDashboard/CountDownTimer/CountDownTimer.jsx
@@ -16,7 +16,11 @@ const CountDownTimer = ({ latestOrderDate }) => {
   useEffect(() => {
     if (latestOrderDate) {
       const startDate = new Date(latestOrderDate);
-      const endDate = new Date(startDate.setMonth(startDate.getMonth() + 3));
+      const endDate = new Date(startDate);
+      endDate.setMonth(endDate.getMonth() + 3);
+      if (endDate.getDate() !== startDate.getDate()) {
+        endDate.setDate(0);
+      }
       setEndTime(endDate.getTime());
     }
   }, [latestOrderDate]);
@@ -29,12 +33,18 @@ const CountDownTimer = ({ latestOrderDate }) => {
 
     let timeLeft = {};
     if (difference > 0) {
+      const months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30.44));  // Średnia długość miesiąca
+      const days = Math.floor((difference % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((difference / 1000 / 60) % 60);
+      const seconds = Math.floor((difference / 1000) % 60);
+      
       timeLeft = {
-        months: Math.floor(difference / (1000 * 60 * 60 * 24 * 30)),
-        days: Math.floor((difference / (1000 * 60 * 60 * 24)) % 30),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
+        months,
+        days,
+        hours,
+        minutes,
+        seconds,
       };
     } else {
       timeLeft = {
@@ -62,17 +72,17 @@ const CountDownTimer = ({ latestOrderDate }) => {
   return (
     <div className={styles.container}>
       <p>
-      Gratulujemy! 
+        Gratulujemy! 
         <a
-        href="#"
-        class="inline-block rounded-full bg-white px-4 py-1 pb-1 pt-1 text-s font-bold leading-normal text-gray-700 transition duration-150 ease-in-out hover:bg-gray-200 ml-2"
-        data-twe-toggle="tooltip"
-        data-twe-placement="top"
-        data-twe-ripple-init
-        data-twe-ripple-color="light"
-        title="Ten wyjątkowy prezent jest dostępny tylko raz na 3 miesiące. Nasze drzewko to unikalny i ponadczasowy prezent!">
-        i
-      </a>
+          href="#"
+          className="inline-block rounded-full bg-white px-4 py-1 pb-1 pt-1 text-s font-bold leading-normal text-gray-700 transition duration-150 ease-in-out hover:bg-gray-200 ml-2"
+          data-twe-toggle="tooltip"
+          data-twe-placement="top"
+          data-twe-ripple-init
+          data-twe-ripple-color="light"
+          title="Ten wyjątkowy prezent jest dostępny tylko raz na 3 miesiące. Nasze drzewko to unikalny i ponadczasowy prezent!">
+          i
+        </a>
       </p>
       <p className={styles.text}>Już niedługo będziesz mieć możliwość obdarowania kolejnej osoby:</p>
       <span>
