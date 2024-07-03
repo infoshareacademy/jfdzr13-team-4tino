3ade0a94437305b0cf448d7fd96010a46a3c01a3
slawomir.rzeszutek <s.rzeszutek@gmail.com>
2024-07-03 12:58:58 +0200

    style mobilne: login,register,PasswordReminder

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -50,7 +50,7 @@ function Register() {
             password: ""
         };
 
-        const nameRegex = /^[A-Za-z]{2,30}$/;
+        const nameRegex = /^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,30}$/;
         const phoneRegex = /(?:\+48)?[\s-]?(\d{3})[\s-]?(\d{3})[\s-]?(\d{3})/;
         const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         const passwordRegex = /^(?=.*[A-Za-z].*[A-Za-z].*[A-Za-z].*[A-Za-z].*[A-Za-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
