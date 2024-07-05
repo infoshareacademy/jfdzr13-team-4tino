2ffc57b3c470da2f2ac553cc509bdf6d46d8b89d
Marta Kozikowska <147720654+martakozikowska@users.noreply.github.com>
2024-07-05 17:41:36 +0200

    yyyy

--- src/components/Homepage/HowItworks/HowItWorks.jsx
+++ src/components/Homepage/HowItworks/HowItWorks.jsx
@@ -7,15 +7,14 @@ const HowItWorks = () => {
     <div className={styles.howItWorksTitle}>
       <div>Jak to działa ?</div>
       <div className={styles.container}>
-        <div>
-            <img src={obrazek} alt="dziewczynka z sadzonką" className={styles.photo}/>
+        <div className={styles.img} alt="dziewczynka z sadzonką">
         </div>
         <div className={styles.paragraphs}>
           <p><span className={styles.highlight}>Wybierz drzewo:</span> skorzystaj z&nbsp;naszej ofert i&nbsp;zdecyduj, które drzewo najlepiej Tobie pasuje.</p>
           <p><span className={styles.highlight}>Wybierz tabliczkę:</span> posiadamy szeroki asortyment różnych tabliczek, na&nbsp;pewno znajdziesz coś dla siebie.</p>
           <p><span className={styles.highlight}>Napisz dedykację:</span> wybierz jedną z&nbsp;naszych propozycji lub&nbsp;napisz coś od&nbsp;serca.</p>
           <p><span className={styles.highlight}>Wybierz lokalizację:</span> posiadamy różne lokalizacje w&nbsp;całym kraju.</p>
-          <p><span className={styles.highlight}>Kup drzewo:</span> My zasadzimy je za Ciebie w wybranej lokalizacji.</p>
+          <p><span className={styles.highlight}>Kup drzewo:</span> My zasadzimy je za Ciebie w&nbsp;wybranej lokalizacji.</p>
           <p><span className={styles.highlight}>Gotowe,</span> zielona przyszłość zaczyna się tutaj! </p>
           <p><span className={styles.highlight}>Uwaga!</span> Kolejne drzewko możesz nabyć dopiero za 3&nbsp;miesiące, więc dobrze przemyśl kogo chcesz obdarować&nbsp;😉</p>
         </div>
