876220902b50d4648f6f8fe62a712ba37b7035f2
Marta Kozikowska <marta.kozikowska@gmail.com>
2024-07-06 16:51:55 +0200

    usuniecie zakomentowanych linijek

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { useUser } from '../../context/UserContext/UserContext';
+import Footer from "../Footer/Footer";
 import About from "../Homepage/About/About";
 import HowItWorks from "../Homepage/HowItworks/HowItWorks";
 import Introduction from "../Homepage/Introduction/Introduction";
 import WhyTrustUs from "../Homepage/WhyTrustUs/WhyTrustUs";
 import styles from "./Homepage.module.css";
-import Footer from "../Footer/Footer"
 
 
 
@@ -13,7 +12,6 @@ const Homepage = () => {
 
   return (
     <div className={styles.homepage}>
-      {/* komponenty here */}
       <Introduction />
       <div id="howItWorks">
         <HowItWorks />
